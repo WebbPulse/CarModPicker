6606faeea7b576fade89380d8d7ca91be1fbbb93
Tyler Webb <tylert2610@gmail.com>
2026-04-06 23:31:32 -0700

    add www. domain

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -76,6 +76,7 @@ class Settings(BaseSettings):
         default=(
             "http://localhost,http://localhost:3000,http://localhost:4000,"
             "https://carmodpicker.com,"
+            "https://www.carmodpicker.com,"
             "https://api.carmodpicker.com,"
             "https://carmodpicker.staging.webbpulse.com,"
             "https://api.carmodpicker.staging.webbpulse.com"
